6522cae07110d5d6fdf24a3a9e14da18a459237e
wonpyo.choi <wonpyo.choi@navercorp.com>
2020-11-18 23:58:03 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        HELLO WORLD
+      </header>
+    </div>
+  );
+}
+
+export default App;
